de3a5f149f3b6c2f146fb607f99c964174597178
JGladines <jona.gladines@uantwerpen.be>
2019-11-25 12:17:47 +0100

    clarify circuit, improve README.md

--- README.md
+++ README.md
@@ -1,13 +1,65 @@
-# SPI-DAC7611
-A library based on the Arduino Platform to use the 12bit DAC by Texas Instruments.
+# Arduino SPI-DAC7611 Library
+
+
+## Description
+This is a small and simple library to control the DAC7611 digital to analog converter (DACs) with an Arduino board.
 
 The DAC7611 is a 12-bit digital-to-analog converter
 with guaranteed 12-bit monotonicity performance over the industrial temperature range. 
 It requires a single +5V supply and contains an input shift register, latch, 2.435V reference, DAC, and high speed rail-to-rail output amplifier. 
 
-This library is enhanced with the SPI library to make use of the speed of the SPI bus of the Arduino.
-  CLK == 13;
-  SDI == 11; 
 
+## Installation
+
+1. Navigate to the Releases page.
+2. Download the latest release.
+3. In the Arduino IDE, navigate to Sketch > Include Library > Add .ZIP Library and select the file downloaded.
+
+You can find more information on installing Arduino libraries [here](https://www.arduino.cc/en/Guide/Libraries).
+
+## Usage Example
 To operate the DAC, first create an instance of the DAC using DAC7611 dac(cs, ld, clr)
 next initialise the dac using dac.begin() and send data with dac.analogWrite(int value)
+
+```
+#include <DAC7611.h>
+
+uint8_t cs = 8;
+uint8_t ld = 2;
+uint8_t clr = 3;
+
+DAC7611 dac(cs, ld, clr);
+
+int dacVal = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  dac.begin();  //initialise DAC
+  Serial.println("Hello");  
+}
+
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    dacVal = Serial.parseInt();
+    dac.analogWrite(dacVal);  //send values to DAC
+    Serial.print("DAC Value is : ");  
+    Serial.println(dacVal);
+  }
+  delay(1000);
+}
+```
+
+## Dependencies
+
+This library depends on the standard [SPI](https://www.arduino.cc/en/Reference/SPI) library.
+
+
+## Licensing
+The code in this project is licensed under MIT license.
+
+
+
+
--- examples/SPI-DAC7611/SPI-DAC7611.ino
+++ examples/SPI-DAC7611/SPI-DAC7611.ino
@@ -1,49 +1,31 @@
+/*
+Release Date: 21-11-2019
+gladeines@gmail.com
 
+The DAC7611 is a 12-bit digital-to-analog converter
+(DAC) with guaranteed 12-bit monotonicity performance over the industrial temperature range. 
+It requires a single +5V supply and contains an input shift
+register, latch, 2.435V reference, DAC, and high speed
+rail-to-rail output amplifier. 
 
-//Release Date: 21-11-2019
-//gladeines@gmail.com
+ The circuit:
+ * DAC7611 attached to SPI bus as follows:
+ ** SDI - pin 11
+ ** CLK - pin 13
+ ** CS - pin 8
+ ** LD - pin 3
+ ** CLR - pin 2
 
-/*===========================================================================
-DAC7611 device library code is placed under the MIT license
-Copyright (c) 2019 Jona Gladines
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-===========================================================================*/
-
-//The DAC7611 is a 12-bit digital-to-analog converter
-//(DAC) with guaranteed 12-bit monotonicity performance over the industrial temperature range. 
-//It requires a single +5V supply and contains an input shift
-//register, latch, 2.435V reference, DAC, and high speed
-//rail-to-rail output amplifier. 
-
-//This library is enhanced with the SPI library to make use of the speed of the SPI bus of the Arduino.
-//CLK == 13;
-//SDI == 11; 
-//To operate the DAC, first create an instance of the DAC using DAC7611 dac(cs, ld, clr)
-//next initialise the dac using begin() and send data 
-//analogWrite(int value)
+To operate the DAC, first create an instance of the DAC using DAC7611 dac(cs, ld, clr)
+next initialise the dac using begin() and send data 
+analogWrite(int value)
+*/
 
 #include <DAC7611.h>
 
 uint8_t cs = 8;
-uint8_t ld = 2;
-uint8_t clr = 3;
+uint8_t ld = 3;
+uint8_t clr = 2;
 
 DAC7611 dac(cs, ld, clr);
 
